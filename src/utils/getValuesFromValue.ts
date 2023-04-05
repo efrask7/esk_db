29066eb1e208cffbe4b582f93d93a9abaf971c17
Efra <efrainschettini.123@gmail.com>
2023-04-04 22:34:44 -0300

    get values from table that equals a value param

---
+++ src/utils/getValuesFromValue.ts
@@ -0,0 +1,45 @@
+import { JSONObject, dbType } from "../types"
+import dbExist from "./dbExists"
+import tableExists from "./tableExists"
+import valueExists from "./valueExists"
+import config from "../config.json"
+import fs from "fs"
+
+const { databasePath } = config
+
+type valueParam = {
+  name: string,
+  value: dbType
+}
+
+const getValuesFromValue = (dbName: string, tableName: string, value: valueParam): Array<JSONObject> => {
+  if (!dbName || !tableName || !value) {
+    throw new Error('You have to pass 3 params')
+  }
+
+  if (!dbExist(dbName)) {
+    throw new Error('The provided database does not exists')
+  }
+
+  if (!tableExists(dbName, tableName)) {
+    throw new Error('The provided table does not exists')
+  }
+
+  if (!valueExists(dbName, tableName, value.name)) {
+    throw new Error('The provided value name does not exists')
+  }
+
+  let returnValues: Array<JSONObject> = []
+
+  const values: Array<JSONObject> = JSON.parse(fs.readFileSync(databasePath + dbName + '/' + tableName + '/data.json', {encoding: 'utf-8'}))
+
+  for (let i in values) {
+    if (values[i][value.name] === value.value) {
+      returnValues.push(values[i])
+    }
+  }
+
+  return returnValues
+}
+
+export default getValuesFromValue
