7c6454e2518b9e14ace956975c26e8888ef87eea
Efra <efrainschettini.123@gmail.com>
2023-04-04 21:58:04 -0300

    exists

---
+++ src/utils/valueExists.ts
@@ -0,0 +1,27 @@
+import dbExist from "./dbExists"
+import tableExists from "./tableExists"
+import fs from "fs"
+import config from "../config.json"
+import { table } from "../types"
+
+const { databasePath } = config
+
+const valueExists = (dbName: string, tableName: string, value: string): boolean => {
+  if (!dbExist(dbName) || !tableExists(dbName, tableName)) {
+    throw new Error('The provided database or table does not exists')
+  }
+
+  const tableInfo: Array<table> = JSON.parse(fs.readFileSync(databasePath + dbName + '/tables.json', {encoding: 'utf-8'}))
+
+  const index = tableInfo.findIndex(v => v.name === tableName)
+
+  const exists = tableInfo[index].values.find(v => v.name === value)
+
+  if (!exists) {
+    return false
+  }
+
+  return true
+}
+
+export default valueExists
